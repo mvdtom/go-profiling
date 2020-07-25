404f3c02d53f46ab9e6b6499666382b57e51be21
Mikhail Derevyashkin <mvdtom@gmail.com>
2020-07-25 15:07:18 +0700

    5071488 ns/op          492948 B/op       6626 allocs/op | added easyjson codegeneration

--- fast.go
+++ fast.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 	"regexp"
 	"strings"
+
+	"github.com/mailru/easyjson/jlexer"
+	"github.com/mailru/easyjson/jwriter"
 )
 
 type User struct {
@@ -23,16 +25,23 @@ func FastSearch(out io.Writer) {
 	}
 	defer file.Close()
 
-	seenBrowsers2 := make(map[string]byte)
+	seenBrowsers2 := make(map[string]byte, 115)
 
 	fmt.Fprintln(out, "found users:")
 	scanner := bufio.NewScanner(file)
+	sBuff := make([]byte, 0, 800)
+	scanner.Buffer(sBuff, 1000)
 	i := 0
 	user := User{}
+	l := jlexer.Lexer{}
+
 	for scanner.Scan() {
 		line := scanner.Bytes()
+		l.Data = line
 		//user := &User{}
-		err := json.Unmarshal(line, &user)
+		//err := json.Unmarshal(line, &user)
+
+		err := user.UnmarshalJSON(line)
 		if err != nil {
 			panic(err)
 		}
@@ -67,4 +76,117 @@ func processUser(out io.Writer, user *User, seenBrowsers map[string]byte, i int)
 	}
 	email := r.ReplaceAllString(user.Email, " [at] ")
 	fmt.Fprintf(out, "[%d] %s <%s>\n", i, user.Name, email)
+}
+
+func easyjson9f2eff5fDecodeCourseraHwHw3BenchDto(in *jlexer.Lexer, out *User) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeFieldName(false)
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "name":
+			out.Name = string(in.String())
+		case "email":
+			out.Email = string(in.String())
+		case "browsers":
+			if in.IsNull() {
+				in.Skip()
+				out.Browsers = nil
+			} else {
+				in.Delim('[')
+				if out.Browsers == nil {
+					if !in.IsDelim(']') {
+						out.Browsers = make([]string, 0, 4)
+					} else {
+						out.Browsers = []string{}
+					}
+				} else {
+					out.Browsers = (out.Browsers)[:0]
+				}
+				for !in.IsDelim(']') {
+					var v1 string
+					v1 = string(in.String())
+					out.Browsers = append(out.Browsers, v1)
+					in.WantComma()
+				}
+				in.Delim(']')
+			}
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+func easyjson9f2eff5fEncodeCourseraHwHw3BenchDto(out *jwriter.Writer, in User) {
+	out.RawByte('{')
+	first := true
+	_ = first
+	{
+		const prefix string = ",\"name\":"
+		out.RawString(prefix[1:])
+		out.String(string(in.Name))
+	}
+	{
+		const prefix string = ",\"email\":"
+		out.RawString(prefix)
+		out.String(string(in.Email))
+	}
+	{
+		const prefix string = ",\"browsers\":"
+		out.RawString(prefix)
+		if in.Browsers == nil && (out.Flags&jwriter.NilSliceAsEmpty) == 0 {
+			out.RawString("null")
+		} else {
+			out.RawByte('[')
+			for v2, v3 := range in.Browsers {
+				if v2 > 0 {
+					out.RawByte(',')
+				}
+				out.String(string(v3))
+			}
+			out.RawByte(']')
+		}
+	}
+	out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v User) MarshalJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	easyjson9f2eff5fEncodeCourseraHwHw3BenchDto(&w, v)
+	return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v User) MarshalEasyJSON(w *jwriter.Writer) {
+	easyjson9f2eff5fEncodeCourseraHwHw3BenchDto(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *User) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson9f2eff5fDecodeCourseraHwHw3BenchDto(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjson9f2eff5fDecodeCourseraHwHw3BenchDto(l, v)
 }
